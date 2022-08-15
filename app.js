fe1fb00c75d81f6bacef2242885837955ce12697
victorudenna@gmail.com <victorudenna@gmail.com>
2022-08-15 15:49:53 +0100

    Fixed faq issue

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const faq_head = document.getElementsByClassName('content-container');
+// const faq_head = document.getElementsByClassName('content-container');
 
-for (i = 0; i < faq_head.length; i++) {
+// for (i = 0; i < faq_head.length; i++) {
 
-    faq_head[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-    })
-};
+//     faq_head[i].addEventListener('click', function() {
+//         this.classList.toggle('active');
+//     })
+// };
+
+let faq_head = document.querySelector('.faq_container');
+
+faq_head.addEventListener('click', function (e) {
+    const target = e.target;
+    
+    if (target.matches('question')) {
+        classList.add('active');
+    }
+})
