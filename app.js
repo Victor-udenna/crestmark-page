70b3effb4b8440edb08d46dff07e77bd9d1057b2
victorudenna@gmail.com <victorudenna@gmail.com>
2022-08-15 21:44:40 +0100

    Changed javascript code

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const faq_head = document.getElementsByClassName('content-container');
+// const faq_head = document.getElementsByClassName('content-container');
 
-for (i = 0; i < faq_head.length; i++) {
+// for (i = 0; i < faq_head.length; i++) {
 
-    faq_head[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-    })
-};
+//     faq_head[i].addEventListener('click', function() {
+//         this.classList.toggle('active');
+//     })
+// };
+
+
+let answers = document.querySelectorAll(".content-container");
+answers.forEach((event) => {
+event.addEventListener("click", () => {
+	if (event.classList.contains("active")) {
+	event.classList.remove("active");
+	} else {
+	event.classList.add("active");
+	}
+});
+});
